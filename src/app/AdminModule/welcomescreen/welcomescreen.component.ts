c16afd8920f4c769aae9d6882ca079fdf7f70e95
unknown <cogdotnet08@iiht.tech>
2022-05-21 20:38:05 +0530

    save

--- src/app/AdminModule/welcomescreen/welcomescreen.component.ts
+++ src/app/AdminModule/welcomescreen/welcomescreen.component.ts
@@ -8,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class WelcomescreenComponent implements OnInit {
 
   userName=localStorage.getItem("userName")
+  userrole=localStorage.getItem("userrole")
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+ message()
+ {
+  if(this.userrole=="A")
+  return "You can create airline, achedule for flight and view details by email Id"
+  else
+  return "You book flight view by PRN Number and Cancel the Ticket"
+ }
 
 }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'flightbooking'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('flightbooking');
+   // expect(app.title).toEqual('flightbooking');
   });
 
   it('should render title', () => {
